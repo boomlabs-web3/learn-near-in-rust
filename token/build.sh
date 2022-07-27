92dbf9553422615ff53e533d3c23f79e6bb452e4
C0wJay <poppinjaewoo@gmail.com>
2022-07-28 00:09:37 +0900

    Start from prvious branch

---
+++ token/build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+set -e && RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release && mkdir -p ./export && cp target/wasm32-unknown-unknown/release/*.wasm ./export/main.wasm
