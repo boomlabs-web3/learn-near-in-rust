6a4747f2f1513874a12cea6e67e268e7c6272152
C0wJay <poppinjaewoo@gmail.com>
2022-07-30 21:27:07 +0900

    minor fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,29 @@
-mod contract;
-pub use contract::*;
+use near_sdk::{
+    borsh::{self, BorshDeserialize, BorshSerialize},
+    log, near_bindgen, PanicOnDefault,
+};
 
-#[allow(dead_code, unused)]
-#[cfg(test)]
-mod tests {
-    use near_sdk::{test_utils::*, testing_env, AccountId, ONE_NEAR};
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Counter {
+    value: u64,
+}
 
-    fn contract_account() -> AccountId {
-        "contract".parse::<AccountId>().unwrap()
+#[near_bindgen]
+impl Counter {
+    #[init]
+    pub fn new(value: u64) -> Self {
+        log!("Custom counter initialization!");
+        Self { value }
     }
-
-    fn get_context(predecessor_account_id: AccountId) -> VMContextBuilder {
-        let mut builder = VMContextBuilder::new();
-        builder
-            .current_account_id(contract_account())
-            .account_balance(15 * ONE_NEAR)
-            .signer_account_id(predecessor_account_id.clone())
-            .predecessor_account_id(predecessor_account_id);
-        builder
+    
+    #[payable]
+    pub fn increment(&mut self) {
+        self.value += 1;
     }
-
-    #[test]
-    fn test() {}
+    
+    #[private]
+    pub fn get_count(&self) -> u64 {
+        self.value
+    }
 }
